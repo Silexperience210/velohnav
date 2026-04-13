d24dfabe44edcbe9a2a9996848c41bcbc9448e3a
Silexperience <122946337+Silexperience210@users.noreply.github.com>
2026-04-13 15:29:00 +0200

    AR navigation module

--- android/app/src/main/java/com/silexperience/velohnav/MainActivity.java
+++ android/app/src/main/java/com/silexperience/velohnav/MainActivity.java
@@ -1,5 +1,13 @@
 package com.silexperience.velohnav;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.silexperience.velohnav.ar.ArNavigationPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ArNavigationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
